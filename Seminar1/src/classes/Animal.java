c9e71d6f0be255781227efd8ac4421ee7f71df74
DraghiaCosmina <drgcosmina@yahoo.com>
2021-02-25 09:13:45 +0200

    Giraffe&Zebra

--- Seminar1/src/classes/Animal.java
+++ Seminar1/src/classes/Animal.java
@@ -2,10 +2,12 @@ package classes;
 
 public class Animal {
 	private String name;
+	private float greutate;
 
-	public Animal(String name) {
+	public Animal(String name, float greutate) {
 		super();
 		this.name = name;
+		this.greutate=greutate;
 	}
 
 	public String getName() {
@@ -14,6 +16,14 @@ public class Animal {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public float getGreutate() {
+		return greutate;
+	}
+
+	public void setGreutate(float greutate) {
+		this.greutate = greutate;
 	}
 	
 
--- Seminar1/src/main/Main.java
+++ Seminar1/src/main/Main.java
@@ -8,6 +8,23 @@ public class Main {
 		Zoo zoo = new Zoo();
 		zoo.setZookeeper(keeper);
 		
+		Giraffe giraffe1 = new Giraffe("Girafa1",10);
+		Giraffe giraffe2=new Giraffe("Girafa2", 15,200);
+		
+		zoo.addAnimal(giraffe1);
+		zoo.addAnimal(giraffe2);
+		
+		zoo.feedAllAnimals();
+		
+		Zebra zebra1 = new Zebra("Zebra 1", 7);
+		Zebra zebra2 = new Zebra("Zebra 2", 10,132);
+		
+		zoo.addAnimal(zebra1);
+		zoo.addAnimal(zebra2);
+		
+		zoo.feedAllAnimals();
+		
+		
 
 	}
 
