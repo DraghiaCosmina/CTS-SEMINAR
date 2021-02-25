faeb45cabfacfc30b5b96664b382b8e2df6be644
DraghiaCosmina <drgcosmina@yahoo.com>
2021-02-25 09:17:10 +0200

    Giraffe

---
+++ Seminar1/src/classes/Giraffe.java
@@ -0,0 +1,18 @@
+package classes;
+
+public class Giraffe extends Animal {
+	
+	private float inaltime;
+
+	public Giraffe(String name, float greutate) {
+		super(name, greutate);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Giraffe(String name, float greutate, float inaltime) {
+		super(name, greutate);
+		this.inaltime = inaltime;
+	}
+	
+
+}
