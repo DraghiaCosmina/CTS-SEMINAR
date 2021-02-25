54a2120a26742e71cbf4596ab324abef1e7a7641
DraghiaCosmina <drgcosmina@yahoo.com>
2021-02-25 08:56:15 +0200

    Classes

--- Seminar1/src/main/Main.java
+++ Seminar1/src/main/Main.java
@@ -1,9 +1,13 @@
 package main;
-
+import classes.*;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.print("Seminar1");
+		//System.out.print("Seminar1");
+		Zookeeper keeper = new Zookeeper("Dani");
+		Zoo zoo = new Zoo();
+		zoo.setZookeeper(keeper);
+		
 
 	}
 
