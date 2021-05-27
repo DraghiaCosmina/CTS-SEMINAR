1a869301b478f5c81b9996347f96c776da5695c0
DraghiaCosmina <drgcosmina@yahoo.com>
2021-05-27 09:11:49 +0300

    test

--- Seminar13/src/ro/ase/cts/test/GrupaTest.java
+++ Seminar13/src/ro/ase/cts/test/GrupaTest.java
@@ -10,15 +10,15 @@ import ro.ase.cts.classes.Grupa;
 import ro.ase.cts.classes.Student;
 
 public class GrupaTest {
-	Grupa grupa ;
+//	Grupa grupa;
 	@Before
 	public void setUp() throws Exception {
-		grupa = new Grupa(1010);
-		for (int i = 1; i < 36; i++) {
-			Student student = new Student();
-			student.adaugaNota(i % 10 + 1);
-			grupa.adaugaStudent(student);
-		}
+//		grupa = new Grupa(1010);
+//		for (int i = 1; i < 36; i++) {
+//			Student student = new Student();
+//			student.adaugaNota(i % 10 + 1);
+//			grupa.adaugaStudent(student);
+//		}
 	}
 
 	@After
@@ -96,10 +96,19 @@ public class GrupaTest {
 		assertEquals(0, grupa.getPromovabilitate(), 0.001f);
 	}
 
-	@Test(timeout = 100)
-	public void performantaPromovabilitateTest() {
-			
-		grupa.getPromovabilitate();
+//	@Test(timeout = 100)
+//	public void performantaPromovabilitateTest() {
+//		grupa.getPromovabilitate();
+//	}
+//	
+	@Test
+	public void cardinalityPromovabilitateTest() {
+		Grupa grupa = new Grupa(1050);
+		Student student = new Student();
+		student.adaugaNota(5);
+		grupa.adaugaStudent(student);
+		
+		assertEquals(1f, grupa.getPromovabilitate(),0.001f);
 	}
 }
 	
